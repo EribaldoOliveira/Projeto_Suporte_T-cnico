7b2f70d3a2b9b7b26182b747e82b1e9c0c01874d
Eribaldo Oliveira <desenvolvedorwebst@gmail.com>
2023-09-19 14:17:24 -0300

    Autenticando usuário

--- valida_login.php
+++ valida_login.php
@@ -1,5 +1,35 @@
 <?php 
-	
+
+	//Variável que verifica se a autenticação foi realizada
+	$usuario_autenticado = false;
+
+	//USUARIOS DO SISTEMA
+$usuarios_app = array(
+    array('email' => 'adm@teste.com.br', 'senha' => '123456'),
+    array('email' => 'user@teste.com.br', 'senha' => 'abcd')
+);
+
+/*
+	echo '<pre>';
+	print_r($usuarios_app);
+	echo '</pre>';
+
+*/
+
+	foreach ($usuarios_app as $user) {
+		
+		if ($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']) {
+			$usuario_autenticado = true;
+		}
+		
+	}
+
+	if($usuario_autenticado){
+		echo 'Usuario_autenticado';
+
+	} else {
+		header('Location: index.php?login=erro');
+	}
 	/*
 	print_r($_GET);
 
@@ -9,7 +39,7 @@
 	echo  '<br />';
 	echo $_GET['senha'];
 */
-
+/*
 	print_r($_POST);
 
 	echo  '<br />';
@@ -17,4 +47,5 @@
 	echo $_POST['email'];
 	echo  '<br />';
 	echo $_POST['senha'];
+	*/
  ?>
