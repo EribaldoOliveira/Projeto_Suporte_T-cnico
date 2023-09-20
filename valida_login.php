c1eed537ab180f65bdaa7367dc33abe8a99d097e
Eribaldo Oliveira <desenvolvedorwebst@gmail.com>
2023-09-20 09:28:42 -0300

    Protegendo páginas restritas com SESSION

--- valida_login.php
+++ valida_login.php
@@ -1,51 +1,58 @@
-<?php 
+<?php
 
-	//Variável que verifica se a autenticação foi realizada
-	$usuario_autenticado = false;
+session_start();
+$_SESSION['x'] = 'Oi, eu sou um valor de sessão!';
+print_r($_SESSION);
+echo '<hr/>';
+print_r($_SESSION['y']);
 
-	//USUARIOS DO SISTEMA
+
+//VARIAVEL QUE VERIFICA SE A AUTENTICAÇÃO FOI REALIZADA
+$usuario_autenticado = false;
+
+//USUARIOS DO SISTEMA
 $usuarios_app = array(
     array('email' => 'adm@teste.com.br', 'senha' => '123456'),
     array('email' => 'user@teste.com.br', 'senha' => 'abcd')
 );
-
 /*
-	echo '<pre>';
-	print_r($usuarios_app);
-	echo '</pre>';
+
+echo '<pre>';
+print_r($usuarios_app);
+echo '</pre>';
 
 */
 
-	foreach ($usuarios_app as $user) {
-		
-		if ($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']) {
-			$usuario_autenticado = true;
-		}
-		
-	}
+foreach($usuarios_app as $user){
+    /*
+    echo 'Usuario app: ' . $user['email'] . '/' . $user['senha'];
+    echo '<br />';
+    echo 'Usuario form: ' . $_POST['email'] . '/' . $_POST['senha'];
+    echo '<hr />';
+    */
+    if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']){
+        $usuario_autenticado = true;
+    }
+}
 
-	if($usuario_autenticado){
-		echo 'Usuario_autenticado';
+    if($usuario_autenticado){
+        echo 'Usuário autenticado.';
 
-	} else {
-		header('Location: index.php?login=erro');
-	}
-	/*
-	print_r($_GET);
+        $_SESSION['autenticado'] = 'SIM';
+    }else{  
+        $_SESSION['autenticado'] = 'NÃO';
+        header('Location: index.php?login=erro');
+      }
 
-	echo  '<br />';
 
-	echo $_GET['email'];
-	echo  '<br />';
-	echo $_GET['senha'];
-*/
 /*
-	print_r($_POST);
+print_r($_GET)
 
-	echo  '<br />';
+echo '<br />'
+echo $_GET['email']
+echo '<br />'
+echo $_GET['senha']
+print_r($_POST);
+*/
 
-	echo $_POST['email'];
-	echo  '<br />';
-	echo $_POST['senha'];
-	*/
- ?>
+?>
